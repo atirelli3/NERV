6e39113fdbaa4c58a99e534028fb5e20a7115272
Andrea Tirelli <ta.tirelliandrea@gmail.com>
2024-12-02 23:01:52 +0100

    started the nerv main script

---
+++ nerv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# todo: opzioni supportate {add, update (ALL), remove (nome)}
+
+# Constants
+#
+OPTIONS=("add" "update" "remove")
+
+# Read args from stdin
+option=$1
+second_arg=$2
+
+
+
+# todo delle opzioni 
